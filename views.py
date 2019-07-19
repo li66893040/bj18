47a88c5d1130a316c302d46434ee3831a12e034b
li66893040 <66893040@qq.com>
2019-07-19 22:19:34 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shorcuts import redirect
 
 def views(request):
     return HttpResponse('IS-OK')
+
+def login(request):
+    return redirect('/views')
